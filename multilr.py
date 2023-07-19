0a2d5bdc09fe73a313f545ab07a545108c6c676c
kardasbart <95145661+kardasbart@users.noreply.github.com>
2023-07-19 15:49:57 +0200

    Update multilr.py
    Fixed some issues

--- multilr.py
+++ multilr.py
@@ -3,27 +3,32 @@ import torch
 class MultiLR(torch.optim.lr_scheduler._LRScheduler):
     def __init__(self, optimizer, lambda_factories, last_epoch=-1, verbose=False):
         self.schedulers = []
-        for factory in lambda_factories:
+        values = self._get_optimizer_lr(optimizer)
+        for idx, factory in enumerate(lambda_factories):
             self.schedulers.append(factory(optimizer))
+            values[idx] = self._get_optimizer_lr(optimizer)[idx]
+            self._set_optimizer_lr(optimizer, values)
         super().__init__(optimizer, last_epoch, verbose)
 
     def get_lr(self):
         result = []
         for idx, sched in enumerate(self.schedulers):
-            result.append(sched.get_lr()[idx])
+            result.append(sched.get_last_lr()[idx])
         return result
+    
+    @staticmethod
+    def _set_optimizer_lr(optimizer, values):
+        for param_group, lr in zip(optimizer.param_groups, values):
+            param_group['lr'] = lr
 
-    def _get_closed_form_lr(self):
-        result = []
-        for idx, sched in enumerate(self.schedulers):
-            if hasattr(self, "_get_closed_form_lr"):
-                values = sched._get_closed_form_lr()
-            else:
-                values = sched.get_lr()
-            result.append(values[idx])
-        return result
+    @staticmethod
+    def _get_optimizer_lr(optimizer):
+        return [group['lr'] for group in optimizer.param_groups]
 
     def step(self, epoch=None):
-        for sched in self.schedulers:
-            sched.step(epoch)
-        super().step(epoch)
+        values = self._get_optimizer_lr(self.optimizer)
+        for idx, sched in enumerate(self.schedulers):
+            sched.step()
+            values[idx] = self._get_optimizer_lr(self.optimizer)[idx]
+            self._set_optimizer_lr(self.optimizer, values)
+        super().step()
